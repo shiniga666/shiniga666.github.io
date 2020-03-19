e73490c24aeab94fff4fc2c98c69607e57f1463e
shiniga666 <dimas.pptsi@gmail.com>
2020-03-19 10:47:53 +0000

    Updates

---
+++ precache-manifest.96c483eb530f64ebbeb9171c38f7b86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9937739449368663a6135eb76d2512f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c50dff153d8f694e587c",
+    "url": "/static/css/main.413a0a58.chunk.css"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/static/js/2.734ad3c6.chunk.js"
+  },
+  {
+    "revision": "4482739f3a8ff521b2955664b1fec844",
+    "url": "/static/js/2.734ad3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50dff153d8f694e587c",
+    "url": "/static/js/main.26ae3ab3.chunk.js"
+  },
+  {
+    "revision": "5e7b69124d0735283b36",
+    "url": "/static/js/runtime-main.b95774aa.js"
+  }
+]);
